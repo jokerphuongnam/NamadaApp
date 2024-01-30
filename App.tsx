3f6296e1468f1f15f540ea8268c586aa116a677b
DESKTOP-SK549TU\LilMonster <phuongnamp99@gmail.com>
2024-01-31 01:31:14 +0700

    Add namada depedencies

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Query, Sdk } from '@namada/shared';
+import { useEffect } from 'react';
+import DashboardView from './views/dashboard_view';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <DashboardView/>
     </View>
   );
 }
