e66b3ef04c5586c7a4f1dbc746a8881093bf77eb
P. Nam <pnam@192.168.2.4>
2024-02-04 13:16:58 +0700

    import namada

--- packages/shared/src/init-inline.ts
+++
@@ -1,4 +0,0 @@
-import initWasm, { InitOutput } from "./shared/shared";
-import wasm from "./shared/shared_bg.wasm";
-
-export const init: () => Promise<InitOutput> = async () => await initWasm(wasm);
